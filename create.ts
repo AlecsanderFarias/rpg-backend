a76be31aa4d6b30dbb4564e796bab8b83a9f7813
AlecsanderFarias <alecs@devfarias.com>
2021-05-05 20:50:52 -0300

    first commit

---
+++ create.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express'
+import User from '../../../models/User';
+
+
+const create = async (req: Request, res: Response) => {
+  try {
+
+
+    const user = await User.create({
+      name: 'Alecsander Farias',
+      email: 'alecs@devfarias.com',
+      birth: new Date(),
+      password: '123456'
+    });
+
+
+    return res.status(201).json({ teste: true });
+  } catch (error) {
+
+    console.error('deu merda');
+
+
+    return res.status(500).json(error);
+  }
+};
+
+export default create;
